989e9d30dc82af1fc04e64e00a05f0a4fdb5bed7
Sergey Uchaev <dialoggg1994@gmail.com>
2023-12-12 23:44:30 +0300

    refactoring and add yarn

---
+++ packages/host/src/root/Pages.tsx
@@ -0,0 +1,22 @@
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Main } from "../pages/Main";
+import { MainLayout } from "@host/layouts/MainLayout";
+
+const Cards = React.lazy(() => import("remote-modules-cards/Cards"));
+const Transactions = React.lazy(() => import("remote-modules-transactions/Transactions"));
+
+const Pages = () => {
+    return <Router>
+        <MainLayout>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/'} element={<Main/>}/>
+                    <Route path={'/cards/*'} element={<Cards />} />
+                    <Route path={'/transactions/*'} element={<Transactions />} />
+                </Routes>
+            </Suspense>
+        </MainLayout>
+    </Router>
+}
+export default Pages;
