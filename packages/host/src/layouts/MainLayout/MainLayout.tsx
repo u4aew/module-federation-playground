ef7e297005a88df0e13ab711bf979c313e22fbca
Sergey Uchaev <dialoggg1994@gmail.com>
2023-12-27 23:02:22 +0300

    Add select role and fix vieport

--- packages/host/src/layouts/MainLayout/MainLayout.tsx
+++ packages/host/src/layouts/MainLayout/MainLayout.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-import { Layout, Button, Row, Col, theme } from 'antd';
+import { Layout, Button, Row, Col, Select, theme } from 'antd';
 import Nav from '@host/components/Nav/Nav';
 const { Header, Sider, Content } = Layout;
 import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
 import { useSelector } from 'react-redux';
 import { userSelector } from '@host/store/features/account/selectors';
+
+const { Option } = Select;
 
 export const MainLayout = ({ children }) => {
   const [collapsed, setCollapsed] = useState(false);
@@ -12,6 +14,11 @@ export const MainLayout = ({ children }) => {
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+
+  const handleRoleChange = (newRole) => {
+    console.log('Selected Role:', newRole);
+    // Handle role change here (e.g., update state or make API call)
+  };
 
   return (
     <Layout style={{ minHeight: '100vh' }}>
@@ -36,7 +43,16 @@ export const MainLayout = ({ children }) => {
             </Col>
             <Col flex="auto" style={{ textAlign: 'right', paddingRight: 24 }}>
               <span>
-                <b>NAME</b>: {user?.name} <b>ROLE</b>:{user?.role}
+                <b>NAME</b>: {user?.name}
+                <Select
+                  defaultValue={user?.role}
+                  style={{ width: 120, marginLeft: 10 }}
+                  onChange={handleRoleChange}
+                >
+                  <Option value="manager">Manager</Option>
+                  <Option value="admin">Admin</Option>
+                  <Option value="user">User</Option>
+                </Select>
               </span>
             </Col>
           </Row>
